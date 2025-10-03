f05563e91f71d7f7c384af77f5ff6a1dba519922
Thiago Macedo <119901046+o-thiago@users.noreply.github.com>
2025-10-03 17:52:55 -0400

    Adicionado build de produção para o flake.nix

--- flake.nix
+++ flake.nix
@@ -15,13 +15,26 @@
         inputs.process-compose-flake.flakeModule
       ];
       perSystem =
+        let
+        in
         {
           pkgs,
           config,
+          lib,
           ...
         }:
+        let
+          packageJson = builtins.fromJSON (builtins.readFile ./package.json);
+          pname = packageJson.name;
+          version = packageJson.version;
+          buildInputs = with pkgs; [
+            nodejs_20
+          ];
+          nativeBuildInputs = buildInputs;
+          npmDepsHash = "sha256-LmqXK2d7ePMAiQ/G5UbdZ82i8CgpXpyo1JIm56HSlOk=";
+        in
         {
-          process-compose."default" =
+          process-compose.development =
             { ... }:
             {
               imports = [
@@ -45,12 +58,39 @@
 
           devShells.default = pkgs.mkShell {
             inputsFrom = [
-              config.process-compose."default".services.outputs.devShell
+              config.process-compose."development".services.outputs.devShell
             ];
-            packages = [
-              pkgs.nodejs
-              pkgs.biome
-            ];
+            packages =
+              buildInputs
+              ++ (with pkgs; [
+                biome
+                prefetch-npm-deps
+              ]);
+          };
+
+          packages.default = pkgs.buildNpmPackage {
+            inherit
+              pname
+              version
+              buildInputs
+              npmDepsHash
+              nativeBuildInputs
+              ;
+
+            src = ./.;
+
+            postInstall = ''
+              mkdir -p $out/bin
+              exe="$out/bin/${pname}"
+              lib="$out/lib/node_modules/${pname}"
+              cp -r ./.next $lib
+              touch $exe
+              chmod +x $exe
+              echo "#!/usr/bin/env bash
+                cd $lib
+                ${pkgs.nodePackages_latest.nodejs}/bin/npm run start
+              " > $exe
+            '';
           };
         };
     };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,8 @@
 import "../lib/oprc.server";
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SessionProvider from "./SessionProvider";
-
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
 	title: "Regras do GPMecatrônica",
@@ -27,9 +16,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body className={`antialiased`}>
 				<SessionProvider>{children}</SessionProvider>
 			</body>
 		</html>
